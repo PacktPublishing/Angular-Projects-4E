e9154a310a1d3695c6f68b5d2e1c1d5b0aae2a1e
Aristeidis Bampakos <bampakoa@gmail.com>
2026-04-11 15:09:27 +0300

    Upgrade Chapter 3 to v22 next

--- ch03/src/app/app.config.ts
+++ ch03/src/app/app.config.ts
@@ -8,6 +8,17 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "easymenu-27809", appId: "1:543617414683:web:ca9c41f7b47c6d1fb46abe", storageBucket: "easymenu-27809.firebasestorage.app", apiKey: "AIzaSyB9o93tNlTlXWyHUeRgT4zkxBdy-CqnTKQ", authDomain: "easymenu-27809.firebaseapp.com", messagingSenderId: "543617414683" })), provideFirestore(() => getFirestore())
+    provideRouter(routes),
+    provideFirebaseApp(() => 
+      initializeApp({
+        projectId: 'easymenu-27809',
+        appId: '1:543617414683:web:ca9c41f7b47c6d1fb46abe',
+        storageBucket: 'easymenu-27809.firebasestorage.app',
+        apiKey: 'AIzaSyB9o93tNlTlXWyHUeRgT4zkxBdy-CqnTKQ',
+        authDomain: 'easymenu-27809.firebaseapp.com',
+        messagingSenderId: '543617414683'
+      })
+    ),
+    provideFirestore(() => getFirestore())
   ]
 };
